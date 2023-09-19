32f9f7695350de318b7c3e6fcc6784760c6d5839
Camilo Calderón <cristian.calderontapia@idartes.gov.co>
2023-09-19 12:09:26 -0500

    Ajustes url produccion

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:3333/api"; // reemplazar con la URL real del backend
+const API_BASE = "https://laboratorio-back.azurewebsites.net/api"; // reemplazar con la URL real del backend
 
 export const postData = async <T>(endpoint: string, data: any): Promise<T> => {
   const response = await fetch(`${API_BASE}${endpoint}`, {
